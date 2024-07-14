178e35ce2f5bd5579a2da0092715192d8d9aff6a
Rafli Haidar <rafli.haidar30@gmail.com>
2024-07-14 10:44:31 +0700

    feat (server) : add error handling interfaces

---
+++ server/app/common/code.ts
@@ -0,0 +1,33 @@
+export interface ResponseCodeValue {
+  code: number;
+  msg: string;
+}
+
+export const RESPONSE_CODE = {
+  SUCCESS: { code: 200, msg: "OK" },
+  BAD_REQUEST: {
+    code: 400,
+    msg: "Invalid request. Recheck the parameters sent",
+  },
+  UNAUTHORIZED: {
+    code: 401,
+    msg: "Access denied. Recheck your credentials or log in",
+  },
+  FORBIDDEN: {
+    code: 403,
+    msg: "You do not have permission to access this resource",
+  },
+  NOT_FOUND: { code: 404, msg: "Requested resource not found" },
+  INTERNAL_SERVER_ERROR: {
+    code: 500,
+    msg: "An internal error occurred on the server. Please try again later",
+  },
+  EMPTY_EMAIL_OR_PASSWORD: {
+    code: 400,
+    msg: "Email or password cannot be empty",
+  },
+  INVALID_SIGNIN: {
+    code: 401,
+    msg: "Invalid email or password. Please check your credentials and try again",
+  },
+};
