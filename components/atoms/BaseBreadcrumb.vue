4715f25c5c70cb8a78b1ae712b3a3c28f16aa308
raflihaidar <rafli.haidar30@gmail.com>
2024-07-21 08:44:01 +0700

    fixed (breadcrumb) : change logic

--- components/atoms/BaseBreadcrumb.vue
+++ components/atoms/BaseBreadcrumb.vue
@@ -1,31 +1,55 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
-import { useRoute } from "vue-router";
-import type { RouteRecordNameGeneric } from "vue-router";
+import { ref, watch, computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
+const router = useRouter();
 
-const page = ref<RouteRecordNameGeneric | null>(
-  route.name === "index" ? "Dashboard" : route.name
-);
+const getUrl = computed(() => {
+  const fullPath = route.fullPath;
+  let temp = "";
+  const routes = fullPath.substring(1).split("/");
 
-watch(
-  () => route.name,
-  (newName: any) => {
-    page.value = newName === "index" ? "Dashboard" : newName;
-  }
-);
+  return routes
+    .map((path: string) => {
+      if (path) {
+        temp = `${temp}/${path}`;
+        return router.resolve(temp);
+      } else {
+        temp = "/";
+        return router.resolve(temp);
+      }
+    })
+    .filter(Boolean);
+});
+
+watchEffect(() => {
+  console.log(getUrl.value);
+});
 </script>
 
 <template>
-  <div class="max-w-40">
-    <section class="text-sm flex gap-x-2 text-color3">
-      <span>Pages</span>
-      <span>/</span>
-      <span class="font-medium text-black capitalize">{{ page }}</span>
-    </section>
-    <section>
-      <p class="text-md font-bold capitalize">{{ page }}</p>
-    </section>
+  <div class="max-w-48">
+    <ul class="flex items-center">
+      <li
+        v-for="(item, index) in getUrl"
+        :key="index"
+        class="flex gap-x-2 items-center ml-2"
+      >
+        <Icon name="ic:round-arrow-forward-ios" />
+        <NuxtLink
+          v-if="item.name"
+          :to="{ name: item.name }"
+          :class="[
+            getUrl.length - 1 === index
+              ? 'font-bold'
+              : 'font-semibold text-color3 ',
+            'capitalize',
+          ]"
+        >
+          {{ item.name }}
+        </NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
