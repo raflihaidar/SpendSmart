7009e319878a9b31527869beef4c448acd6c1a0c
raflihaidar <rafli.haidar30@gmail.com>
2024-07-21 09:32:39 +0700

    style (server-middleware) : change unused code

--- server/middleware/dateValidate.ts
+++ server/middleware/dateValidate.ts
@@ -5,19 +5,13 @@ export default defineEventHandler(async (event) => {
     return;
 
   const body = await readBody(event);
-  console.log("CONNECT");
 
   if (!body) {
-    console.log("error di sini");
     throw createError({
       statusCode: RESPONSE_CODE.BAD_REQUEST.code,
       statusMessage: RESPONSE_CODE.BAD_REQUEST.msg,
     });
   }
-
-  console.log(body.description);
-  console.log(body.createdAt);
-  console.log(typeof body.category_id);
 
   body.createdAt = new Date(body.createdAt).toISOString();
 });
